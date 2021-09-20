b662583b9872f27da4fb4ba13282af84b7e83370
Macedo <cleitonrt3@gmail.com>
2021-09-20 19:28:28 -0300

    aula 2 e aula 3

---
+++ src/aula2/OperadoresLogicos.java
@@ -0,0 +1,10 @@
+package aula2;
+
+public class OperadoresLogicos {
+    public static void main(String[] args){
+        boolean comparador = 10 <= 20;
+        System.out.println(comparador);
+        System.out.println(5 == 5);
+        System.out.println(15 != 15);
+    }
+}
---
+++ src/aula3/OperadoresAtribuicao.java
@@ -0,0 +1,9 @@
+package aula3;
+
+public class OperadoresAtribuicao {
+    public static void main(String[] args) {
+        int salario = 18;
+        salario += 1000;
+        System.out.println(salario);
+    }
+}
