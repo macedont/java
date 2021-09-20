def69ba1450a7ac49eb15d9f0250982d58e4a4ba
Macedo <cleitonrt3@gmail.com>
2021-09-19 22:24:54 -0300

    aula1

---
+++ src/aula1/primeiraAula.java
@@ -0,0 +1,13 @@
+package aula1;
+
+public class primeiraAula {
+    
+    public static void main(String[] args){
+        String nome = "Macedo";
+        String endereco = "rua dos bobos nº 0";
+        String telefone = "+55 (00)123456";
+
+        System.out.println("O " + nome + "domiciliado no endereço " + endereco + " e telefone " + telefone +
+        " não possui nenhum tipo de pendencia");
+    }
+}
