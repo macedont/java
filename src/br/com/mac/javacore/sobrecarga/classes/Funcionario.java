562df887d8d4bd30340effed981989a01f845e04
Macedo <brw.cmacedo@ifood3rd.com>
2021-10-07 11:24:17 -0300

    sobrecarga de metodos

---
+++ src/br/com/mac/javacore/sobrecarga/classes/Funcionario.java
@@ -0,0 +1,57 @@
+package br.com.mac.javacore.sobrecarga.classes;
+
+public class Funcionario {
+    private String nome;
+    private String cpf;
+    private double salario;
+    private String rg;
+
+    public void init(String nome, String cpf, double salario){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
+
+    public void init(String nome, String cpf, double salario, String rg){
+        init(nome, cpf, salario);
+        this.rg = rg;
+    }
+
+    public void imprimir(){
+        System.out.println(getNome());
+        System.out.println(getCpf());
+        System.out.println(getSalario());
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+
+    public void setRg(String rg){
+        this.rg = rg;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getCpf(){
+        return this.cpf;
+    }
+
+    public double getSalario(){
+        return this.salario;
+    }
+
+    public String getRg(){
+        return this.rg;
+    }
+}
---
+++ src/br/com/mac/javacore/sobrecarga/test/FuncionarioTest.java
@@ -0,0 +1,12 @@
+package br.com.mac.javacore.sobrecarga.test;
+
+import br.com.mac.javacore.sobrecarga.classes.Funcionario;
+
+public class FuncionarioTest {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.init("Macedo", "000.222.444-66", 2000, "1.123.456");
+
+        funcionario.imprimir();
+    }
+}
